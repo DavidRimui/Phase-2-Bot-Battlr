098eefd949aef4aab542535b56f618bb4420e13e
¨kericho¨ <lvisrono90@gmail.com>
2023-01-23 11:43:04 +0300

    add a bootstrap

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -9,10 +9,15 @@ function YourBotArmy(props) {
 
   return (
     <div className="ui form">
+      <nav className="navbar bg-dark" data-bs-theme="dark">
+        <h1>Bots</h1>
+</nav>
       <div className="ui five column grid">
         <div className="row bot-army-row">
           {bots}
-          {bots.length === 0 ? <h1>My Bots Army</h1> : null}
+          {bots.length === 0 ? <h1>My Bots Display
+          
+          </h1> : null}
         </div>
       </div>
     </div>
